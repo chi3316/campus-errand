2e4dd1f228aa5a4c96a9fe6cfa0c42b7083787a6
黄铭涛 <114841534+1037827920@users.noreply.github.com>
2024-05-30 17:41:42 +0800

    feat: ui (#6)

---
+++ pages/cooperative-complaint/cooperative-complaint.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="weixin">
+		<image class="cover-image" mode="widthFix" src="https://web-cjpdemo.oss-cn-guangzhou.aliyuncs.com/55f6f01850df7a174a8e7df7b4af3a2.jpg">
+		</image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.weixin {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 10rpx 0;
+		width: 100%;
+	}
+	.custom-cover {
+		position: relative;
+	}
+	.cover-image {
+		border-radius: 16rpx; /* 设置圆角半径 */
+	}
+</style>
