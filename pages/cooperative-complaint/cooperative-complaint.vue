7eca785e23316e5905f97f9b1d25ea959e48b95d
Jamie <jamiechen.778@gmail.com>
2024-05-31 18:58:55 +0800

    更新合作页面

--- pages/cooperative-complaint/cooperative-complaint.vue
+++ pages/cooperative-complaint/cooperative-complaint.vue
@@ -1,37 +1,53 @@
 <template>
-	<view class="weixin">
-		<image class="cover-image" mode="widthFix" src="https://web-cjpdemo.oss-cn-guangzhou.aliyuncs.com/55f6f01850df7a174a8e7df7b4af3a2.jpg">
-		</image>
+	<view class="container">
+		<view class="notice-bar-wrapper">
+			<uni-notice-bar v-if="content" show-icon speed="50" scrollable :text="content" />
+		</view>
+		<view class="weixin">
+			<image class="cover-image" mode="widthFix"
+				src="https://web-cjpdemo.oss-cn-guangzhou.aliyuncs.com/55f6f01850df7a174a8e7df7b4af3a2.jpg">
+			</image>
+		</view>
+
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
+export default {
+	data() {
+		return {
+			content: '请添加管理员微信洽谈合作~'
 		}
+	},
+	methods: {
+
 	}
+}
 </script>
 
 <style>
-	.weixin {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		padding: 10rpx 0;
-		width: 100%;
-	}
-	.custom-cover {
-		position: relative;
-	}
-	.cover-image {
-		border-radius: 16rpx; /* 设置圆角半径 */
-	}
+.custom-cover {
+	position: relative;
+}
+
+.cover-image {
+	border-radius: 16rpx;
+	/* 设置圆角半径 */
+}
+
+.container {
+	display: flex;
+	flex-direction: column;
+	/* 垂直排列子元素 */
+	align-items: center;
+	/* 子元素水平居中对齐 */
+	padding: 0 20rpx;
+	/* 在容器两侧添加间距 */
+}
+
+.notice-bar-wrapper {
+	width: 100%;
+	margin-top: 20rpx;
+	margin-bottom: 20rpx;
+}
 </style>
--- pages/feedback/feedback.vue
+++ pages/feedback/feedback.vue
@@ -1,37 +1,41 @@
 <template>
 	<view class="weixin">
-		<image class="cover-image" mode="widthFix" src="https://web-cjpdemo.oss-cn-guangzhou.aliyuncs.com/7eb9615e856bbaaf233089d27787cfb.jpg">
+
+		<image class="cover-image" mode="widthFix"
+			src="https://web-cjpdemo.oss-cn-guangzhou.aliyuncs.com/7eb9615e856bbaaf233089d27787cfb.jpg">
 		</image>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
+export default {
+	data() {
+		return {
 		}
+	},
+	methods: {
+
 	}
+}
 </script>
 
 <style>
-	.weixin {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		padding: 10rpx 0;
-		width: 100%;
-	}
-	.custom-cover {
-		position: relative;
-	}
-	.cover-image {
-		border-radius: 16rpx; /* 设置圆角半径 */
-	}
+.weixin {
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 10rpx 0;
+	width: 100%;
+}
+
+.custom-cover {
+	position: relative;
+}
+
+.cover-image {
+	border-radius: 16rpx;
+	/* 设置圆角半径 */
+}
 </style>
