9c2557652941cd0fdb8bbe7cc2fb01f38ff737b9
黄铭涛 <1037827920@qq.com>
2024-06-01 23:56:20 +0800

    feat: 实现profile中所有静态页面

---
+++ pages/my-discount-coupon/my-discount-coupon.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="container">
+		<view class="segmented-control-wrap">
+			<uni-segmented-control :current="current" :values="items" :style-type="styleType"
+				:active-color="activeColor"/>
+		</view>
+		<view class="empty-wrap">
+			<fui-empty src="https://web-cjpdemo.oss-cn-guangzhou.aliyuncs.com/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20240601113716.png" isFixed="true" width="733"></fui-empty>
+		</view>
+	</view>
+</template>
+
+<script>
+import fuiEmpty from "@/components/firstui/fui-empty/fui-empty.vue"
+
+export default {
+	components: {
+		fuiEmpty,
+	},
+	data() {
+		return {
+			items: ['待使用', '已使用', '已过期'],
+			styles: [{
+				value: 'button',
+				text: '按钮',
+				checked: true
+			},
+			{
+				value: 'text',
+				text: '文字'
+			}
+			],
+			current: 0,
+			colorIndex: 0,
+			activeColor: '#007aff',
+			styleType: 'text',
+		}
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<style>
+.container {
+	background-color: #f8f8f8;
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
+}
+.segmented-control-wrap {
+	flex-shrink: 0;
+}
+.empty-wrap {
+	flex: 1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
---
+++ pages/rank/rank.vue
@@ -0,0 +1,51 @@
+<template>
+	<view>
+		<view class="box">
+			<uni-row>
+				<uni-col :span="12">
+					<text>开始日期</text>
+				</uni-col>
+				<uni-col :span="12">
+					<uni-datetime-picker type="date" :clear-icon="false" v-model="single" @maskClick="maskClick" />
+				</uni-col>
+			</uni-row>
+			<uni-row>
+				<uni-col :span="12">
+					<text>结束日期</text>
+				</uni-col>
+				<uni-col :span="12" style="margin-top: 25rpx;">
+					<uni-datetime-picker type="date" :clear-icon="false" v-model="single" @maskClick="maskClick" />
+				</uni-col>
+			</uni-row>
+			<text>
+				总数：{{ count }}位
+			</text>
+		</view>
+		<view>
+			<uni-card title="用户名" :isFull="true" sub-title="完成单数: 1" extra="金额: 1000" :thumbnail="avatar"></uni-card>
+			<uni-card title="用户名" :isFull="true" sub-title="完成单数: 1" extra="金额: 1000" :thumbnail="avatar"></uni-card>
+
+		</view>
+	</view>
+
+</template>
+
+<script>
+import fuiAvatar from "@/components/firstui/fui-avatar/fui-avatar.vue"
+export default {
+	components: {
+		fuiAvatar,
+	},
+	data() {
+		return {
+			count: 2,
+			avatar: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png',
+		}
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<style></style>
