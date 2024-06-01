d3f64e2ee1de36caa25470a20b904d10cbe0dfd5
Jamie <jamiechen.778@gmail.com>
2024-06-01 20:04:52 +0800

    Page address: 获取后台数据地址并新增地址

--- pages/add-address/add-address.vue
+++ pages/add-address/add-address.vue
@@ -2,16 +2,16 @@
 	<view class="add-address">
 		<uni-forms :modelValue="formData">
 			<uni-forms-item label="楼栋" required name="building">
-				<uni-easyinput v-model="formData.building" placeholder="请输入楼栋"/>
+				<uni-easyinput v-model="formData.building" placeholder="请输入楼栋" />
 			</uni-forms-item>
-			<uni-forms-item label="门牌号" name="house_number" required>
-				<uni-easyinput v-model="formData.house_number" placeholder="请输入门牌号" />
+			<uni-forms-item label="门牌号" name="specificLocation" required>
+				<uni-easyinput v-model="formData.specificLocation" placeholder="请输入门牌号" />
 			</uni-forms-item>
-			<uni-forms-item label="姓名" name="name" required>
-				<uni-easyinput v-model="formData.name" placeholder="请输入姓名" />
+			<uni-forms-item label="姓名" name="consignee" required>
+				<uni-easyinput v-model="formData.consignee" placeholder="请输入姓名" />
 			</uni-forms-item>
-			<uni-forms-item label="手机号" name="phone_number" required>
-				<uni-easyinput v-model="formData.phone_number" placeholder="请输入手机号" />
+			<uni-forms-item label="手机号" name="phone" required>
+				<uni-easyinput v-model="formData.phone" placeholder="请输入手机号" />
 			</uni-forms-item>
 		</uni-forms>
 		<fui-button radius="96rpx" @click="submitAddress" color="white" background="#37CBE8">确定</fui-button>
@@ -19,36 +19,51 @@
 </template>
 
 <script>
-	import fuiButton from "@/components/firstui/fui-button/fui-button.vue"
-	export default {
-		components: {
-			fuiButton,
-		},
-		data() {
-			return {
-				formData: {
-					building: '', // 楼栋
-					house_number: '', // 门牌号
-					name: '', // 姓名
-					phone_number: '', //手机号
-				},
-			}
-		},
-		methods: {
-			submitAddress() {
-				this.$bus.$emit('add-address', this.formData);
-				uni.navigateBack();
+import fuiButton from "@/components/firstui/fui-button/fui-button.vue"
+export default {
+	components: {
+		fuiButton,
+	},
+	data() {
+		return {
+			formData: {
+				building: '', // 楼栋
+				specificLocation: '', // 门牌号
+				consignee: '', // 姓名
+				phone: '', //手机号
 			},
 		}
+	},
+	methods: {
+		submitAddress() {
+			this.$request.put("/user/address/save", this.formData).then((res) => {
+				console.log(res)
+				if (res.code === 1) {
+					uni.showToast({
+						title: '添加成功',
+						duration: 2000
+					});
+				}
+			})
+			uni.navigateBack({
+				delta: 1, // 返回上一级页面
+				success: function () {
+					const page = getCurrentPages().pop();
+					if (page == undefined || page == null) return;
+					page.onLoad({ refresh: true });
+				}
+			});
+		},
 	}
+}
 </script>
 
 <style>
-	.add-address {
-		background-color: #fff;
-		padding: 20rpx;
-		border-radius: 10rpx;
-		box-shadow: 0 4rpx 20rpx rgba(0,0,0,.1);
-		margin: 10rpx;
-	}
+.add-address {
+	background-color: #fff;
+	padding: 20rpx;
+	border-radius: 10rpx;
+	box-shadow: 0 4rpx 20rpx rgba(0, 0, 0, .1);
+	margin: 10rpx;
+}
 </style>
--- pages/address/address.vue
+++ pages/address/address.vue
@@ -4,15 +4,17 @@
 			<fui-empty title="暂无数据"></fui-empty>
 		</view>
 		<view v-else class="address-content">
-			<fui-card v-for="item in items" :key="item.id" :title="`${item.building}-${item.house_number}`" radius="30rpx" :size="35" :margin="['25rpx', '10rpx']">
+			<fui-card v-for="item in items" :key="item.id" :title="`${item.building}-${item.specificLocation}`"
+				radius="30rpx" :size="35" :margin="['25rpx', '10rpx']">
 				<view class="fui-card-content">
-					{{ item.name }}   {{ item.phone_number }}
+					{{ item.consignee }} {{ item.phone }}
 					<view class="buttons-container">
 						<view class="edit-button">
 							<fui-button background="#37CBE8" width="100rpx" height="70rpx">编辑</fui-button>
 						</view>
 						<view class="delete-button">
-							<fui-button background="#EEEEEE" color="#DE1E54" border-color="#0099FF" width="100rpx" height="70rpx" @click="removeAddress(item.id)">删除</fui-button>
+							<fui-button background="#EEEEEE" color="#DE1E54" border-color="#0099FF" width="100rpx"
+								height="70rpx" @click="removeAddress(item.id)">删除</fui-button>
 						</view>
 					</view>
 				</view>
@@ -26,106 +28,110 @@
 </template>
 
 <script>
-	import fuiButton from "@/components/firstui/fui-button/fui-button.vue"
-	import fuiEmpty from "@/components/firstui/fui-empty/fui-empty.vue"
-	import fuiCard from "@/components/firstui/fui-card/fui-card.vue"
-	
-	export default {
-		components: {
-			fuiButton,
-			fuiEmpty,
-			fuiCard,
+import fuiButton from "@/components/firstui/fui-button/fui-button.vue"
+import fuiEmpty from "@/components/firstui/fui-empty/fui-empty.vue"
+import fuiCard from "@/components/firstui/fui-card/fui-card.vue"
+
+export default {
+	components: {
+		fuiButton,
+		fuiEmpty,
+		fuiCard,
+	},
+	onLoad(options) {
+		if (options.refresh) {
+			this.loadAddressData(); // 调用方法刷新地址数据
+		}
+	},
+	created() {
+		this.loadAddressData()
+	},
+	mounted() {
+		this.isEmpty = this.items.length === 0
+	},
+	data() {
+		return {
+			isEmpty: true,
+			items: [],
+		}
+	},
+	methods: {
+		jumpAddAdress() {
+			console.log("跳转新增地址页面");
+			uni.navigateTo({
+				url: '/pages/add-address/add-address', // 跳转页面
+			})
 		},
-		created() {
-			this.$bus.$on('add-address', this.addAddress);
-		},
-		mounted() {
-			this.isEmpty = this.items.length === 0
-		},
-		data() {
-			return {
-				isEmpty: true,
-				items: [
-					{id: '1', building: '楼栋x', house_number: 'xxx', name: '姓名', phone_number: '123456'},
-				],
-			}
-		},
-		methods: {
-			jumpAddAdress() {
-				console.log("跳转新增地址页面");
-				uni.navigateTo({
-					url: '/pages/add-address/add-address', // 跳转页面
-				})
-			},
-			addAddress(newAddress) {
-				console.log("添加地址");
-				// 示例
-				// const newAddress = {id: this.items.length+1, building: '楼栋x', room: 'xxx', name: '姓名', phone: "123456"};
-				this.items.push({...newAddress, id: this.items.length+1});
+		loadAddressData() {
+			// 初始化数据
+			this.items = [];
+			// 请求地址数据
+			this.$request.get("/user/address/list").then((res) => {
+				// 将获取到的地址数据赋值给 items 数组
+				this.items = res.data;
+				// 更新 isEmpty 的值
 				this.isEmpty = this.items.length === 0;
-			},
-			removeAddress(id) {
-				console.log("删除地址");
-				this.items = this.items.filter(item => item.id != id);
-				this.isEmpty = this.items.length === 0;
-			}
+			}).catch((error) => {
+				console.error('获取地址数据失败：', error);
+			});
 		}
 	}
+}
 </script>
 
 <style>
-	.address-container {
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-	}
-	
-	.empty-container {
-		width: 100%;
-		display: flex;
-		justify-content: center;
-	}
-	
-	.address-content {
-		padding: 10px 0;
-	}
-	
-	.divider {
-		display: flex;
-		width: 100%;
-		height: 1px;
-		background-color: #37CBE8;
-		margin-top: 1100rpx;
-	}
-	
-	.add-address {
-		display: flex;
-		flex-direction: column;
-		padding: 10px 0;
-		margin-top: 1px;
-		margin-left: 10px;
-		margin-right: 10px;
-	}
-	
-	.fui-card-content {
-		position: relative;
-		display: flex;
-		margin-left: 10px;
-		height: 40px;
-	}
-	
-	.buttons-container {
-		position: absolute;
-		right: 10px;
-		display: flex;
-		align-items: center;
-	}
-	
-	.edit-button {
-		margin-left: 10px;
-	}
-	
-	.delete-button {
-		margin-left: 5px;
-	}
+.address-container {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+}
+
+.empty-container {
+	width: 100%;
+	display: flex;
+	justify-content: center;
+}
+
+.address-content {
+	padding: 10px 0;
+}
+
+.divider {
+	display: flex;
+	width: 100%;
+	height: 1px;
+	background-color: #37CBE8;
+	margin-top: 1100rpx;
+}
+
+.add-address {
+	display: flex;
+	flex-direction: column;
+	padding: 10px 0;
+	margin-top: 1px;
+	margin-left: 10px;
+	margin-right: 10px;
+}
+
+.fui-card-content {
+	position: relative;
+	display: flex;
+	margin-left: 10px;
+	height: 40px;
+}
+
+.buttons-container {
+	position: absolute;
+	right: 10px;
+	display: flex;
+	align-items: center;
+}
+
+.edit-button {
+	margin-left: 10px;
+}
+
+.delete-button {
+	margin-left: 5px;
+}
 </style>
